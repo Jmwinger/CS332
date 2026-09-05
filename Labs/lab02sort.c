2d75766c64d718abd24c2cb8cc179b813e00f463
Jmwinger <jmwinger@uab.edu>
2026-09-05 03:00:38 +0000

    sorted list

--- Labs/lab02sort.c
+++ Labs/lab02sort.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main (int ){
-    int sort_arr [3];
+int main (){
+    int arr[3];
 
-    printf ("Enter three integers")
+    printf ("Enter three integers: ");
+    scanf ("%d %d %d", &arr[0], &arr[1], &arr[2]);
+
+    printf ("Given elements are: [%d, %d, %d]\n", arr[0], arr[1], arr[2]);
+
+
+    for (int i = 1; i < 3; i++){
+        int currLoc = i;
+        while (currLoc > 0 && arr[currLoc - 1] > arr[currLoc]){
+            int temp = arr[currLoc];
+            arr[currLoc] = arr[currLoc - 1];
+            arr[currLoc - 1] = temp;
+            currLoc--;
+        }
+    }
+
+    printf ("Sorted array: [%d, %d, %d]\n", arr[0], arr[1], arr[2]);
+
+    return 0;
 }
