8bd7367543b9233b699ad29f79c9eaf5dd5b2507
Jmwinger <jmwinger@uab.edu>
2026-09-13 13:40:23 +0000

    modify insertion sort

--- Labs/lab03modifysort.c
+++ Labs/lab03modifysort.c
@@ -1,11 +1,80 @@
-#include <stdio.h> 
-#include <string.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char** argv) { 
-    char hello[20] = {'h','e','l','l','o',' ','\0'}; 
-    char name[] = "students!\0"; 
-    strcat(hello, name); 
-    printf("%s\n",hello); 
+void readStrings(char **arr, int size);
+void sortStrings(char **arr, int size);
+void displayStrings(char **arr, int size);
 
-    return 0; 
-} 
+int main() {
+    int N;
+
+    printf("Enter number of strings: ");
+    scanf("%d", &N);
+
+    /* Allocate memory for array of string pointers */
+    char **arr = (char **)malloc(N * sizeof(char *));
+    if (arr == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    readStrings(arr, N);
+
+    printf("\nOriginal Strings:\n");
+    displayStrings(arr, N);
+
+    sortStrings(arr, N);
+
+    printf("\nSorted Strings:\n");
+    displayStrings(arr, N);
+
+    /* Free allocated memory */
+    for (int i = 0; i < N; i++) {
+        free(arr[i]);
+    }
+    free(arr);
+
+    return 0;
+}
+
+/* Read strings and allocate memory dynamically */
+void readStrings(char **arr, int size) {
+    char temp[100];
+
+    for (int i = 0; i < size; i++) {
+        printf("Enter string %d: ", i + 1);
+        scanf("%99s", temp);
+
+        arr[i] = (char *)malloc((strlen(temp) + 1) * sizeof(char));
+
+        if (arr[i] == NULL) {
+            printf("Memory allocation failed.\n");
+            exit(1);
+        }
+
+        strcpy(arr[i], temp);
+    }
+}
+
+/* Display strings */
+void displayStrings(char **arr, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%s\n", arr[i]);
+    }
+}
+
+/* Insertion Sort for strings */
+void sortStrings(char **arr, int size) {
+    for (int i = 1; i < size; i++) {
+        char *key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && strcmp(arr[j], key) > 0) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
